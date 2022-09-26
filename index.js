ebad235340fd0e930ad98a2a49a04caef576cfe0
Mara Hernandez <mara.hesp@gmail.com>
2022-09-26 18:06:28 -0500

    first-commit

--- index.js
+++ index.js
@@ -20,11 +20,17 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/:input', function (req, res) {
+  let response = {}
+  response['ipaddress'] = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+  response['language'] = req.headers['accept-language']
+  response['software'] = req.headers['user-agent']
+  res.json(response);
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+let PORT = process.env.PORT || 3000
+
+app.listen(PORT, (req, res) => {
+  console.log(`Your app is listening on port ${PORT}`)
+})
